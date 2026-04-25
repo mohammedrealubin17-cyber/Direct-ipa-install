8c939b386c8c2435de6dd80afb1ada1ff734cba3
mohammedrealubin17-cyber <mohammed.realubin17@gmail.com>
2026-04-25 14:15:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const params = new URLSearchParams(window.location.search);
+const app = params.get("app");
 
+const appName = document.getElementById("appName");
+const installLink = document.getElementById("installLink");
+
+// Example manifests (you must host these yourself)
+const apps = {
+  testapp: {
+    name: "My Test App",
+    manifest: "itms-services://?action=download-manifest&url=https://yourserver.com/testapp.plist"
+  },
+  another: {
+    name: "Another App",
+    manifest: "itms-services://?action=download-manifest&url=https://yourserver.com/another.plist"
+  }
+};
+
+if (apps[app]) {
+  appName.innerText = apps[app].name;
+  installLink.href = apps[app].manifest;
+}
